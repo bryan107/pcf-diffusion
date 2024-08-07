ca8d51fa7c13c90cd23282bf88abbad4bf106629
CoRneLIUs <niels.carioukotlarek@yahoo.com>
2024-08-07 15:12:46 +0100

    cleaned the two NN classes gen and disc. Code runs and model converge.

--- src/networks/generators.py
+++ src/networks/generators.py
@@ -3,26 +3,11 @@ from typing import Tuple
 import torch
 import torch.nn as nn
 
+from src.networks.residualdeepnetwork import ResidualDeepNetwork
 from src.utils import init_weights
 
 
-class GeneratorBase(nn.Module):
-    def __init__(self, input_dim, output_dim):
-        super(GeneratorBase, self).__init__()
-        """ Generator base class. All generators should be children of this class. """
-        self.input_dim = input_dim
-        self.output_dim = output_dim
-
-    def forward_(self, batch_size: int, n_lags: int, device: str):
-        pass
-
-    def forward(self, batch_size: int, n_lags: int, device: str):
-        x = self.forward_(batch_size, n_lags, device)
-        x = self.pipeline.inverse_transform(x)
-        return x
-
-
-class LSTMGenerator(GeneratorBase):
+class LSTMGenerator(nn.Module):
     def __init__(
         self,
         input_dim: int,
@@ -33,120 +18,85 @@ class LSTMGenerator(GeneratorBase):
         BM=False,
         activation=nn.Tanh(),
     ):
-        super(LSTMGenerator, self).__init__(input_dim, output_dim)
-        # LSTM
+        super().__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.rnn_hidden_dim = hidden_dim
+        self.rnn_num_layers = n_layers
+
         self.rnn = nn.LSTM(
             input_size=input_dim,
-            hidden_size=hidden_dim,
-            num_layers=n_layers,
+            hidden_size=self.rnn_hidden_dim,
+            num_layers=self.rnn_num_layers,
             batch_first=True,
         )
-        self.rnn.apply(init_weights)
-        self.linear = nn.Linear(hidden_dim, output_dim, bias=False)
-        self.linear.apply(init_weights)
+        self.linear = nn.Linear(self.rnn_hidden_dim, output_dim, bias=False)
 
         self.initial_nn = nn.Sequential(
-            ResFNN(input_dim, hidden_dim * n_layers, [hidden_dim, hidden_dim]),
-            nn.Tanh(),
-        )  # we use a simple residual network to learn the distribution at the initial time step.
-        self.initial_nn1 = nn.Sequential(
-            ResFNN(input_dim, hidden_dim * n_layers, [hidden_dim, hidden_dim]),
+            ResidualDeepNetwork(
+                input_dim,
+                self.rnn_hidden_dim * self.rnn_num_layers,
+                [self.rnn_hidden_dim, self.rnn_hidden_dim],
+            ),
             nn.Tanh(),
         )
-        self.initial_nn.apply(init_weights)
-        self.initial_nn1.apply(init_weights)
+        self.initial_nn1 = nn.Sequential(
+            ResidualDeepNetwork(
+                input_dim,
+                self.rnn_hidden_dim * self.rnn_num_layers,
+                [self.rnn_hidden_dim, self.rnn_hidden_dim],
+            ),
+            nn.Tanh(),
+        )
 
-        self.BM = BM
+        self.apply(init_weights)
+        self.activation = activation
+
+        # Noise config
+        self.use_bm_as_noise_else_gaussian = BM
         if BM:
             self.noise_scale = noise_scale
         else:
             self.noise_scale = 0.3
-        self.activation = activation
+
+        return
+
+    def get_noise_vector(self, shape: Tuple[int, ...], device: str) -> torch.Tensor:
+        return self.noise_scale * torch.randn(*shape, self.input_dim, device=device)
 
     def forward(
-        self, batch_size: int, n_lags: int, device: str, z=None
+            self,
+            batch_size: int,
+            n_lags: int,
+            device: str,
+            noise_sequence_z: torch.Tensor = None,
     ) -> torch.Tensor:
-        if z == None:
-            z = (self.noise_scale * torch.randn(batch_size, n_lags, self.input_dim)).to(
-                device
-            )  # cumsum(1)
-            if self.BM:
-                z = z.cumsum(1)
-            else:
-                pass
-            # z[:, 0, :] *= 0  # first point is fixed
-            #
-        else:
-            z = z
-        z0 = self.noise_scale * torch.randn(batch_size, self.input_dim, device=device)
+        noise_initial_hidden_states = self.get_noise_vector((batch_size,), device)
 
+        # TODO 07/08/2024 nie_k: I have doubts regarding the usefullness of residual networks here.
         h0 = (
-            self.initial_nn(z0)
-            .view(batch_size, self.rnn.num_layers, self.rnn.hidden_size)
+            self.initial_nn(noise_initial_hidden_states)
+            .view(batch_size, self.rnn_num_layers, self.rnn_hidden_dim)
             .permute(1, 0, 2)
             .contiguous()
         )
         c0 = (
-            self.initial_nn1(z0)
-            .view(batch_size, self.rnn.num_layers, self.rnn.hidden_size)
+            self.initial_nn1(noise_initial_hidden_states)
+            .view(batch_size, self.rnn_num_layers, self.rnn_hidden_dim)
             .permute(1, 0, 2)
             .contiguous()
         )
-        # c0 = torch.zeros_like(h0)
 
-        h1, _ = self.rnn(z, (h0, c0))
-        x = self.linear(self.activation(h1))
+        if noise_sequence_z == None:
+            noise_sequence_z = self.get_noise_vector((batch_size, n_lags), device)
+            if self.use_bm_as_noise_else_gaussian:
+                noise_sequence_z = noise_sequence_z.cumsum(1)
 
-        assert x.shape[1] == n_lags
+        hn, _ = self.rnn(noise_sequence_z, (h0, c0))
+        output = self.linear(self.activation(hn))
 
-        return x
+        assert (
+                output.shape[1] == n_lags
+        ), f"output.shape[1] = {output.shape[1]} != {n_lags} = n_lags"
 
-
-class ResidualBlock(nn.Module):
-    def __init__(self, input_dim: int, output_dim: int):
-        super(ResidualBlock, self).__init__()
-        self.linear = nn.Linear(input_dim, output_dim)
-        self.activation = nn.LeakyReLU()
-        self.create_residual_connection = True if input_dim == output_dim else False
-
-    def forward(self, x):
-        y = self.activation(self.linear(x))
-        if self.create_residual_connection:
-            y = x + y
-        return y
-
-
-class ResFNN(nn.Module):
-    def __init__(
-        self,
-        input_dim: int,
-        output_dim: int,
-        hidden_dims: Tuple[int],
-        flatten: bool = False,
-    ):
-        """
-        Feedforward neural network with residual connection.
-        Args:
-            input_dim: integer, specifies input dimension of the neural network
-            output_dim: integer, specifies output dimension of the neural network
-            hidden_dims: list of integers, specifies the hidden dimensions of each layer.
-                in above definition L = len(hidden_dims) since the last hidden layer is followed by an output layer
-        """
-        super(ResFNN, self).__init__()
-        blocks = list()
-        self.input_dim = input_dim
-        self.flatten = flatten
-        input_dim_block = input_dim
-        for hidden_dim in hidden_dims:
-            blocks.append(ResidualBlock(input_dim_block, hidden_dim))
-            input_dim_block = hidden_dim
-        blocks.append(torch.nn.Tanh())
-        blocks.append(nn.Linear(input_dim_block, output_dim))
-        self.network = nn.Sequential(*blocks)
-        self.blocks = blocks
-
-    def forward(self, x):
-        if self.flatten:
-            x = x.reshape(x.shape[0], -1)
-        out = self.network(x)
-        return out
+        return output
