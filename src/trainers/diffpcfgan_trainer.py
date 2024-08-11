87c7b9a2ef3d55c0fdd764341f8cc8a0593904aa
CoRneLIUs <niels.carioukotlarek@yahoo.com>
2024-08-12 00:36:31 +0100

    add diffpcfgan_trainer.py

--- src/trainers/diffpcfgan_trainer.py
+++ src/trainers/diffpcfgan_trainer.py
@@ -1,3 +1,6 @@
+import typing
+from functools import lru_cache
+
 import torch
 from PIL import ImageFile
 
@@ -13,18 +16,18 @@ from src.trainers.trainer import Trainer
 # TODO 12/08/2024 nie_k: Alternative plot for swiss roll.
 
 
-class PCFGANTrainer(Trainer):
+class DiffPCFGANTrainer(Trainer):
     def __init__(
-        self,
-        generator,
-        config,
-        learning_rate_gen,
-        learning_rate_disc,
-        num_D_steps_per_G_step,
-        num_samples_pcf,
-        hidden_dim_pcf,
-        test_metrics_train,
-        test_metrics_test,
+            self,
+            generator,
+            config,
+            learning_rate_gen,
+            learning_rate_disc,
+            num_D_steps_per_G_step,
+            num_samples_pcf,
+            hidden_dim_pcf,
+            test_metrics_train,
+            test_metrics_test,
     ):
         super().__init__(
             test_metrics_train=test_metrics_train,
@@ -126,7 +129,7 @@ class PCFGANTrainer(Trainer):
             self.evaluate(fake_samples, targets, path)
         return
 
-    def _training_step_gen(self, optim_gen, targets):
+    def _training_step_gen(self, optim_gen, targets: torch.Tensor) -> float:
         optim_gen.zero_grad()
 
         fake_samples = self.sample(
@@ -141,7 +144,7 @@ class PCFGANTrainer(Trainer):
         optim_gen.step()
         return loss_gen.item()
 
-    def _training_step_disc(self, optim_discr, targets):
+    def _training_step_disc(self, optim_discr, targets: torch.Tensor) -> float:
         optim_discr.zero_grad()
 
         with torch.no_grad():
@@ -156,3 +159,40 @@ class PCFGANTrainer(Trainer):
         optim_discr.step()
 
         return loss_disc.item()
+
+    def noise_data(
+            self, starting_data: torch.Tensor, timestep_diffusion: torch.Tensor
+    ) -> typing.Tuple[torch.Tensor, torch.Tensor]:
+        # timestep_diffusion should have values between 0 and diffusion_steps -1, and where 0 corresponds to the initial data.
+        alphas, betas, baralphas = self.get_noise_level()
+
+        eps = torch.randn(size=starting_data.shape)
+
+        portion_original_data = (baralphas[timestep_diffusion] ** 0.5).repeat(
+            1, starting_data.shape[1]
+        ) * starting_data
+        portion_white_noise = ((1 - baralphas[timestep_diffusion]) ** 0.5).repeat(
+            1, starting_data.shape[1]
+        ) * eps
+        noised = portion_original_data + portion_white_noise
+        return noised, eps
+
+    @lru_cache(maxsize=None)
+    def get_noise_level(self) -> typing.Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        diffusion_steps = 50
+        # Set noising variances betas as in Nichol and Dariwal paper (https://arxiv.org/pdf/2102.09672.pdf)
+        s = 0.008
+
+        timesteps = torch.tensor(range(0, diffusion_steps), dtype=torch.float32)
+        schedule = (
+                torch.cos((timesteps / diffusion_steps + s) / (1.0 + s) * torch.pi / 2.0)
+                ** 2
+        )
+
+        # All of shape [diffusion_steps]
+        baralphas: torch.Tensor = schedule / schedule[0]
+        betas: torch.Tensor = 1.0 - baralphas / torch.cat(
+            [baralphas[0:1], baralphas[0:-1]]
+        )
+        alphas: torch.Tensor = 1.0 - betas
+        return alphas, betas, baralphas
