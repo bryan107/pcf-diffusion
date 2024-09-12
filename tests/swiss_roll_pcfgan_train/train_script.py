c1d9f66824f66dc477e54578d82ebbef2eb4aaeb
CoRneLIUs <niels.carioukotlarek@yahoo.com>
2024-09-12 15:46:16 +0100

    minor changes for experiments

--- tests/swiss_roll_pcfgan_train/train_script.py
+++ tests/swiss_roll_pcfgan_train/train_script.py
@@ -32,7 +32,7 @@ from src.utils.utils_os import factory_fct_linked_path, remove_files_from_dir, s
 sns.set()
 seed_everything(142, workers=True)
 
-datamodel_name = "pcfgan_long_diff_combination_loss"
+datamodel_name = "pcfgan_long_diff_scorematching_loss"
 path2file_linker = factory_fct_linked_path(ROOT_DIR, "tests/swiss_roll_pcfgan_train")
 datamodel_path = path2file_linker(["out", datamodel_name, ""])
 filename_model_saved = "pcfgan_1"
@@ -105,7 +105,7 @@ epochs = 5001
 trainer = Trainer(
     default_root_dir=path2file_linker(["out"]),
     # gradient_clip_val=0.1,
-    gpus=[3],
+    gpus=[4],
     max_epochs=epochs,
     logger=[logger_custom],
     check_val_every_n_epoch=period_log,
