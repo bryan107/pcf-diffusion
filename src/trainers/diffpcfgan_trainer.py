507786c8c7dded83e6a13ab14ac42b8e62c4de88
CoRneLIUs <niels.carioukotlarek@yahoo.com>
2024-09-12 13:05:33 +0100

    minor changes for experiments

--- src/trainers/diffpcfgan_trainer.py
+++ src/trainers/diffpcfgan_trainer.py
@@ -396,11 +396,11 @@ class DiffPCFGANTrainer(Trainer):
         loss_gen_reconstruction = self.reconstruction_loss(
             diffused_targets[:, 1, :-1], denoised_diffused_targets[:, 1, :-1]
         )
-        total_loss = loss_gen_reconstruction
+        # total_loss = loss_gen_reconstruction
 
         loss_gen_score_matching = self._compute_score_matching_loss(targets)
         # if self.use_diffusion_score_matching_loss:
-        #     total_loss = total_loss + 0.1 * loss_gen_score_matching
+        total_loss = loss_gen_score_matching
 
         self.manual_backward(total_loss)
         optim_gen.step()
